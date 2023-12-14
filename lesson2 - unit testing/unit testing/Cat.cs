a096d44309233790bb452b218dfbded1957836f5
Borushko Dima <borushkoddima@mail.ru>
2023-12-14 16:56:45 +0300

    Create a simple Console Project with Cat class

---
+++ lesson2 - unit testing/unit testing/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_testing
+{
+    public class Cat
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+}
