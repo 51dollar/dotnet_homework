2c2ef7c1e24fef50b10d68513f22a5f38c26dc28
Borushko Dima <borushkoddima@mail.ru>
2024-01-07 14:43:07 +0300

    add class User and method GetGreeting()

---
+++ Homework Unit Testing/Homework Unit Testing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace User_Greeting
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
---
+++ Homework Unit Testing/Homework Unit Testing/User.cs
@@ -0,0 +1,30 @@
+﻿namespace User_Greeting
+{
+	public class User
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+
+		public User(string name, int age)
+		{
+			if (age < 0)
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+			Name = name;
+			Age = age;
+		}
+
+		public string GetGreeting()
+		{
+			if (Age >= 18)
+			{
+				return $"Привет, {Name}! Добро пожаловать!";
+			}
+			else
+			{
+				return $"Привет, {Name}! Ты слишком молод, чтобы войти.";
+			}
+		}
+	}
+}
