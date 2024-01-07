c91b95f3705908e5667cbfac514edf42dac27fb2
Borushko Dima <borushkoddima@mail.ru>
2024-01-07 19:56:54 +0300

    add decorator pattern

---
+++ Decorator and SOLID/Tesla showroom/CarDecorator/Autopilot.cs
@@ -0,0 +1,16 @@
+﻿using Tesla_showroom.Car;
+
+namespace Tesla_showroom.CarDecorator
+{
+	class Autopilot : ModelSDecorator
+	{
+		public Autopilot(ModelS modelS)
+			: base(modelS.Name + ", Автопилот", modelS)
+		{ }
+
+		public override int GetCost()
+		{
+			return modelS.GetCost() + 6000;
+		}
+	}
+}
---
+++ Decorator and SOLID/Tesla showroom/CarDecorator/FullAutopilot.cs
@@ -0,0 +1,16 @@
+﻿using Tesla_showroom.Car;
+
+namespace Tesla_showroom.CarDecorator
+{
+	class FullAutopilot : ModelSDecorator
+	{
+		public FullAutopilot(ModelS modelS)
+			: base(modelS.Name + ", Полный автопилот", modelS)
+		{ }
+
+		public override int GetCost()
+		{
+			return modelS.GetCost() + 12000;
+		}
+	}
+}
